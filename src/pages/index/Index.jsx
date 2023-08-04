77a121a4bea7877607cf40cf869171223ce36e85
Matias Gabriel Ramirez <matiasgr3009@gmail.com>
2023-08-04 15:33:40 -0300

    carousel component

--- src/pages/index/Index.jsx
+++ src/pages/index/Index.jsx
@@ -1,14 +1,15 @@
 const Index = () => {
     return (
-        <div className="flex flex-row bg-gray-600 text-white justify-center w-3/4 self-center">
+        <div className="flex flex-row border-2 p-5 bg-gray-600 text-white justify-center w-11/12 h-60 self-center">
             <div className="flex flex-col self-center">
                 <h1 className="text-3xl">
                     Find the perfect destination
                 </h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est dolorem vel qui vitae rem minus, labore, amet minima sit hic ullam! Minima temporibus, quaerat voluptatum modi nihil perspiciatis dignissimos beatae!</p>
+                <a href="#" className=" w-2/12 border-spacing-2 border-2 text-center bg-blue-900">Call to action</a>
             </div>
-            <div className="flex flex-col">
-                <img src="" ></img>
+            <div className="flex flex-col w-1/4">
+                <img className="border-4 w-full h-full" src="" alt="loading..."></img>
             </div>
         </div>
     )
